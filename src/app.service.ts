5312d73097df944cf138ec611b1e3afc84da5347
xxxhand <nohandboy@gmail.com>
2025-01-16 08:34:58 +0800

    Add try catch block

--- src/app.service.ts
+++ src/app.service.ts
@@ -37,6 +37,8 @@ interface IWarningInfo {
 export class AppService {
   private _Logger: LoggerService;
   private _isRunning = false;
+  private _now: number;
+  private readonly OUTPUT_FILE_PREFIX: string = 'scanOtaStatus_';
 
   constructor(
     @Inject(DEFAULT_LOGGER_FACTORY)
@@ -51,96 +53,97 @@ export class AppService {
 
   @Cron(appConf.executeExpression)
   async execute(): Promise<void> {
-    if (this._isRunning) {
-      this._Logger.log('Last task is not completed yet...');
-      return;
-    }
-    const warningInfoAry: IWarningInfo[] = [];
-    this._isRunning = true;
-    await this.db.tryConnect();
-    // 取前2小時資料
-    const useTime = new Date(Date.now() - 60 * 60 * 2 * 1000);
-    // const useTime = new Date(2024, 8, 30);
-    this._Logger.log('Start to find abnormal devices------------------------------');
-    const aggr: any = [
-      {
-        $match: {
-          otaStatus: 2,
-          updatedAt: {
-            $gte: useTime,
+
+    try {
+      this._now = Date.now();
+      if (this._isRunning) {
+        this._Logger.log('Last task is not completed yet...');
+        return;
+      }
+
+      const warningInfoAry: IWarningInfo[] = [];
+      this._isRunning = true;
+      await this.db.tryConnect();
+      // 取前2小時資料
+      const useTime = new Date(Date.now() - 60 * 60 * 2 * 1000);
+      // const useTime = new Date(2024, 8, 30);
+      this._Logger.log('Start to find abnormal devices------------------------------');
+      const aggr: any = [
+        {
+          $match: {
+            otaStatus: 2,
+            updatedAt: {
+              $gte: useTime,
+            },
           },
         },
-      },
-      {
-        // 根據 sn 分組，並統計每個 sn 的出現次數
-        $group: {
-          _id: '$sn',
-          count: { $sum: 1 },
+        {
+          // 根據 sn 分組，並統計每個 sn 的出現次數
+          $group: {
+            _id: '$sn',
+            count: { $sum: 1 },
+          },
         },
-      },
-      {
-        // 過濾出現次數大於 1 的 sn
-        $match: {
-          count: { $gt: 3 },
+        {
+          // 過濾出現次數大於 1 的 sn
+          $match: {
+            count: { $gt: 3 },
+          },
         },
-      },
-      {
-        $sort: { _id: 1 },
-      },
-    ];
+        {
+          $sort: { _id: 1 },
+        },
+      ];
 
-    this._Logger.log(JSON.stringify(aggr));
-
-    const gDevices = (await this.db.getCollection('DeviceStatusInfos').aggregate(aggr).toArray()) as IGroupDevice[];
-    if (!CustomValidator.nonEmptyArray(gDevices)) {
-      this._Logger.log('No devices matched....terminated');
-      this._terminate();
-      return;
-    }
-    this._Logger.log(`${gDevices.length} devices were found....`);
-    // Find device
-    let q: any = {
-      sn: {
-        $in: gDevices.map((x) => x._id),
-      },
-    };
-    const oDevices = (await this.db.getCollection('Devices').find(q).toArray()) as unknown as IPartialDevice[];
-    // Find account
-    q = {
-      accountId: {
-        $in: oDevices.map((x) => x.accountId),
-      },
-    };
-    const oAccounts = (await this.db.getCollection('Accounts').find(q).toArray()) as unknown as IPartialAccount[];
-    // compose info
-    for (const gDevice of gDevices) {
-      const currDevice = oDevices.find((x) => x.sn === gDevice._id);
-      if (!currDevice) {
-        this._Logger.log(`Not found sn: ${gDevice._id} in Devices`);
-        continue;
+      // this._Logger.log(JSON.stringify(aggr));
+      const gDevices = (await this.db.getCollection('DeviceStatusInfos').aggregate(aggr).toArray()) as IGroupDevice[];
+      if (!CustomValidator.nonEmptyArray(gDevices)) {
+        this._Logger.log('No devices matched....terminated');
+        this._terminate();
+        return;
       }
-      const currInfo: IWarningInfo = {
-        sn: currDevice.sn,
-        uid: currDevice.uid,
-        iotKey: currDevice.iotKey,
-        accountId: currDevice.accountId,
-        count: gDevice.count,
-        email: '',
+      this._Logger.log(`${gDevices.length} devices were found....`);
+      // Find device
+      let q: any = {
+        sn: {
+          $in: gDevices.map((x) => x._id),
+        },
       };
-      if (!CustomValidator.nonEmptyString(currInfo.accountId)) {
-        warningInfoAry.push(currInfo);
-        continue;
-      }
+      const oDevices = (await this.db.getCollection('Devices').find(q).toArray()) as unknown as IPartialDevice[];
+      // Find account
+      q = {
+        accountId: {
+          $in: oDevices.map((x) => x.accountId),
+        },
+      };
+      const oAccounts = (await this.db.getCollection('Accounts').find(q).toArray()) as unknown as IPartialAccount[];
+      // compose info
+      for (const gDevice of gDevices) {
+        const currDevice = oDevices.find((x) => x.sn === gDevice._id);
+        if (!currDevice) {
+          this._Logger.log(`Not found sn: ${gDevice._id} in Devices`);
+          continue;
+        }
+        const currInfo: IWarningInfo = {
+          sn: currDevice.sn,
+          uid: currDevice.uid,
+          iotKey: currDevice.iotKey,
+          accountId: currDevice.accountId,
+          count: gDevice.count,
+          email: '',
+        };
+        if (!CustomValidator.nonEmptyString(currInfo.accountId)) {
+          warningInfoAry.push(currInfo);
+          continue;
+        }
 
-      const currAccount = oAccounts.find((x) => x.accountId === currInfo.accountId);
-      if (currAccount) {
-        currInfo.email = CustomUtils.fromBase64ToString(currAccount.email);
+        const currAccount = oAccounts.find((x) => x.accountId === currInfo.accountId);
+        if (currAccount) {
+          currInfo.email = CustomUtils.fromBase64ToString(currAccount.email);
+        }
+        warningInfoAry.push(currInfo);
       }
-      warningInfoAry.push(currInfo);
-    }
-    // Send notify....
-    try {
-      const outputFile = `scanOtaStatus_${Date.now().toString()}.csv`;
+      const outputFile = `${this.OUTPUT_FILE_PREFIX}${this._now.toString()}.csv`;
       const csvContent = await new AsyncParser().parse(warningInfoAry).promise();
       await fs.writeFile(`./${outputFile}`, csvContent, {
         encoding: DEFAULT_ENCODING,
@@ -161,16 +164,23 @@ export class AppService {
         }),
       });
       await this.mailClient.send(sendOpt);
-      // fs.unlink(`./${outputFile}`).catch((ex) => this._Logger.error(ex.stack));
-    } catch (ex) {
-      this._Logger.error(ex.stack);
+    } catch (error) {
+      this._Logger.error(error.message);
+      this._Logger.error(error.stack);
     } finally {
       await this._terminate();
     }
   }
+
   private async _terminate(): Promise<void> {
     this._isRunning = false;
     this._Logger.log('End to find abnormal devices------------------------------');
     this.db.close().catch((ex) => this._Logger.error(ex.stack));
+    // Remove old csv files
+    const files = await fs.readdir('./');
+    const csvFiles = files.filter((x) => x.endsWith('.csv')).filter((x) => x !== `${this.OUTPUT_FILE_PREFIX}${this._now.toString()}.csv`);
+    for (const file of csvFiles) {
+      await fs.unlink(file);
+    }
   }
 }
